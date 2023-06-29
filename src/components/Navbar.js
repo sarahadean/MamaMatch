962c82fd61d295c3becff8271fc26d73711b788d
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-06-28 23:20:52 -0500

    material ui basic component added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
-import { AppBar } from '@mui/material'
+import { AppBar, Button, Typography, Box, Toolbar, IconButton} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react'
 
 function Navbar() {
   return (
-    <AppBar>
-        Navvvyyyy
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+           <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     // <div> Navbar
 
     // </div>
