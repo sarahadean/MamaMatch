737fc972f769bf72e50a2fd38d0d026b04835635
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-07-17 13:38:23 -0500

    nav bar styled and functional - need to adust some colors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App.jsx'
 import './index.css'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 )
